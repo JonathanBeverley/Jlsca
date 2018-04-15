0deddf959265ba9886d9aaed36707d2903ca0262
Jonathan Beverley <jonathan.beverley@gmail.com>
2018-04-14 22:54:08 -0400

    get offsets straightened out

--- src/attackaes-ilya.jl
+++ src/attackaes-ilya.jl
@@ -34,9 +34,7 @@ function leak(this::flipHW, intermediate::Union{UInt8,UInt16,UInt32,UInt128})
     typeof(intermediate).size*8 - hw(intermediate)
 end
 
-function numberOfPhases(params::AesIlyaAttack)
-    return 256
-end
+numberOfPhases(params::AesIlyaAttack) = 256
 
 # Need a new target
 type TwoRoundTarget <: Target{UInt8,UInt8,UInt8} end
@@ -48,9 +46,12 @@ show(io::IO, a::TwoRoundTarget) = print(io, "Round input, for r0, that means pla
 
 function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
     global previousKeyByte
-    # We know the key, it is the following, but we don't cheat, so not using it.
-    # 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
-    previousKeyByte = phase
+    # We know the example key, it is the following, but there's no point in cheating.
+    # 2b7e151628aed2a6abf7158809cf4f3c
+
+	# Phase is 1-based, but keyBytes are 0-based
+    previousKeyByte = phase - 1
+
     # We can't set previous key-bytes yet, because we don't know them, do that later
     targetfn = TwoRoundTarget()
     return [targetfn for i in 1:(numberOfTargets(params,phase))]
@@ -61,7 +62,7 @@ numberOfTargets(params::AesIlyaAttack, phase::Int) = 15
 
 # However, we skip the first byte...
 function getTargetOffsets(params::AesIlyaAttack, phase::Int)
-    [i for i in 1:15]
+    [i for i in 2:16]
 end
 
 show(io::IO, a::AesIlyaAttack) = print(io, "AES two-byte Ilya attack")
@@ -75,7 +76,7 @@ end
 function datafilter(data::Vector{UInt8})
     # called once per row, to return the "data" we'll use...
     # we return pairs of xor'd bytes to prep for above...
-    return [ data[i]⊻data[i+1] for i in 1:(length(data)-1)]
+    return [ data[i-1]⊻data[i] for i in 2:(length(data))]
 end
 
 function getDataPass(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
