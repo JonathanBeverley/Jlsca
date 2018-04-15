d29ee3e26b22bc50fe23edafea20d495929d4367
Jonathan Beverley <jonathan.beverley@gmail.com>
2018-04-15 16:58:51 -0400

    Now can determine first byte uniquely; specify previousKeyByte type everywhere

--- src/attackaes-ilya.jl
+++ src/attackaes-ilya.jl
@@ -18,39 +18,33 @@ type AesIlyaAttack <: AesAttack
     end
 end
 
-previousKeyByte = -1
+previousKeyByte = 0x00::UInt8
 
 function ilyaRankCallBack(rankData::RankData, keyOffsets::Vector{Int64})
     global previousKeyByte
     phase = length(rankData.combinedScores)
     target = length(rankData.combinedScores[phase])
     orderedArrayOfFloats = rankData.combinedScores[phase][target]
-    previousKeyByte = indmax(orderedArrayOfFloats)-1
+    maxindex = indmax(orderedArrayOfFloats)-1
+    previousKeyByte = convert(UInt8, maxindex)
 end
 
-type flipHW <: Leakage
-end
+type flipHW <: Leakage end
 function leak(this::flipHW, intermediate::Union{UInt8,UInt16,UInt32,UInt128})
-    typeof(intermediate).size*8 - hw(intermediate)
+    return typeof(intermediate).size*8 - hw(intermediate)
 end
-
-numberOfPhases(params::AesIlyaAttack) = 256
 
 # Need a new target
 type TwoRoundTarget <: Target{UInt8,UInt8,UInt8} end
 function target(a::TwoRoundTarget, data::UInt8, guess::UInt8)
     global previousKeyByte
-    data ⊻ guess ⊻ previousKeyByte
+    return data ⊻ guess ⊻ previousKeyByte
 end
-show(io::IO, a::TwoRoundTarget) = print(io, "Round input, for r0, that means plaintext ⊻ keybyte")   
+show(io::IO, a::TwoRoundTarget) = print(io, "Two-round target: (Pᵢ₋₁ ⊻ Kᵢ₋₁) ⊻ (Pᵢ ⊻ Kᵢ)")
 
 function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
     global previousKeyByte
-    # We know the example key, it is the following, but there's no point in cheating.
-    # 2b7e151628aed2a6abf7158809cf4f3c
-
-	# Phase is 1-based, but keyBytes are 0-based
-    previousKeyByte = phase - 1
+    previousKeyByte = 0x00::UInt8
 
     # We can't set previous key-bytes yet, because we don't know them, do that later
     targetfn = TwoRoundTarget()
@@ -58,12 +52,7 @@ function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8}
 end
 
 # We attack one keybyte at a time, but in 256 ways...
-numberOfTargets(params::AesIlyaAttack, phase::Int) = 15
-
-# However, we skip the first byte...
-function getTargetOffsets(params::AesIlyaAttack, phase::Int)
-    [i for i in 2:16]
-end
+numberOfTargets(params::AesIlyaAttack, phase::Int) = 16
 
 show(io::IO, a::AesIlyaAttack) = print(io, "AES two-byte Ilya attack")
 
@@ -76,7 +65,8 @@ end
 function datafilter(data::Vector{UInt8})
     # called once per row, to return the "data" we'll use...
     # we return pairs of xor'd bytes to prep for above...
-    return [ data[i-1]⊻data[i] for i in 2:(length(data))]
+    # For the first round, use 0x0 as the previous datum
+    return vcat([data[1]], [ data[i-1]⊻data[i] for i in 2:(length(data))])
 end
 
 function getDataPass(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
