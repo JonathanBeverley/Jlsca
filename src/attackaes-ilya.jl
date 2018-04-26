8df3d4ebf38fc09af1104985c64b3eaa31a479b3
Jonathan Beverley <jonathan.beverley@gmail.com>
2018-04-26 00:27:08 -0400

    Many bugfixes, works off-the-shelf for CW and clear A3U traces

--- src/attackaes-ilya.jl
+++ src/attackaes-ilya.jl
@@ -14,12 +14,12 @@ type AesIlyaAttack <: AesAttack
     direction::Direction
     sbox::Vector{UInt8}
     knownKey::Array{UInt8,1}
-    previousKeyByte::UInt8
+    previousKeyByte # untyped to allow use of `nothing`
+    rhme3::Bool
     function AesIlyaAttack()
-        return new(CIPHER, KL128, FORWARD, Aes.sbox, [], 0)
+        return new(CIPHER, KL128, FORWARD, Aes.sbox, [], 0, false)
     end
 end
-
 
 function ilyaRankCallBack(params::AesIlyaAttack, rankData::RankData, keyOffsets::Vector{Int64})
     phase = length(rankData.combinedScores)
@@ -42,29 +42,32 @@ end
 
 # Need a new target
 type TwoRoundTarget <: Target{UInt16,UInt8,UInt8}
-    sbox::Vector{UInt8}
+    params::AesIlyaAttack
 end
 function target(a::TwoRoundTarget, data::UInt16, guess::UInt8)
     prevData = UInt8(data>>8)
     nowData = UInt8(data&0xff)
-    if params.previousKeyByte == nothing
-        return 0xff ⊻ sbox[(nowData ⊻ guess)+1]
+    if a.params.previousKeyByte == nothing
+        return 0xff ⊻ a.params.sbox[(nowData ⊻ guess)+1]
     else
-        return (nowData ⊻ guess) ⊻ (prevData ⊻ params.previousKeyByte) 
+        return (nowData ⊻ guess) ⊻ (prevData ⊻ a.params.previousKeyByte)
     end
 end
 show(io::IO, a::TwoRoundTarget) = print(io, "Two-round target: (Pᵢ₋₁ ⊻ Kᵢ₋₁) ⊻ (Pᵢ ⊻ Kᵢ)")
 
 function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
-    # params.previousKeyByte = UInt8(phase-1)
-    params.previousKeyByte = nothing
+    if params.rhme3
+        params.previousKeyByte = nothing
+    else
+        params.previousKeyByte = UInt8(phase-1)
+    end
 
     # We can't set previous key-bytes yet, because we don't know them, do that later
-    targetfn = TwoRoundTarget(params.sbox)
+    targetfn = TwoRoundTarget(params)
     return [targetfn for i in 1:(numberOfTargets(params,phase))]
 end
 
-# We attack one keybyte at a time, but in 256 ways...
+# Optional, ideally it should just work, but we might have to brute-force the first byte
 numberOfPhases(params::AesIlyaAttack) = 1
 numberOfTargets(params::AesIlyaAttack, phase::Int) = 16
 
@@ -74,26 +77,28 @@ function printParameters(params::AesIlyaAttack)
     @printf("mode:       %s\n", string(params.mode))
     @printf("key length: %s\n", string(params.keyLength))
     @printf("direction:  %s\n", string(params.direction))
+    @printf("known key:  %s\n", string(params.knownKey))
 end
 
 function recoverKey(params::AesIlyaAttack, phaseInputOrig::Vector{UInt8})
-    result =  reshape(Aes.InvShiftRows(reshape(phaseInputOrig[1:16],(4,4))), 16)
+    if params.rhme3
+        result =  reshape(Aes.InvShiftRows(reshape(phaseInputOrig[1:16],(4,4))), 16)
+    else
+        result = phaseInputOrig
+    end
     for i in 1:min(length(result),length(params.knownKey))
         result[i] = params.knownKey[i]
     end
     return result
 end
 
-function rhme3Filter(data::Vector{UInt8})
-    shifted = reshape(Aes.ShiftRows(reshape(data[1:16],(4,4))), 16)
-    return twoRoundFilter(shifted)
-end
-
-function twoRoundFilter(data::Vector{UInt8})
+function twoRoundFilter(params::AesIlyaAttack, data::Vector{UInt8})
     # called once per row, to return the "data" we'll use...
     # we return pairs of xor'd bytes to prep for above...
     # For the first round, use 0x0 as the previous datum
-    #return [(UInt16(data[3])<<8)|data[i] for i in 1:16]
+    if params.rhme3
+        data = reshape(Aes.ShiftRows(reshape(data[1:16],(4,4))), 16)
+    end
     return vcat(
                 [UInt16(data[1])],
                 [(UInt16(data[i-1])<<8)|data[i] for i in 2:16]
@@ -101,6 +106,6 @@ function twoRoundFilter(data::Vector{UInt8})
 end
 
 function getDataPass(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
-    return Nullable(x -> rhme3Filter(x))
+    return Nullable(x -> twoRoundFilter(params, x))
 end
 
