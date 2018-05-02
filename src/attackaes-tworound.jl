85769b37b50697a98da339ffc00633616fc9ad16
JonathanBeverley <jonathan.beverley@gmail.com>
2018-05-02 17:20:04 -0400

    Better class names

--- src/attackaes-tworound.jl
+++ src/attackaes-tworound.jl
@@ -1,14 +1,27 @@
-# This file is a Jlsca implementation of Ilya Kizhvatov's XMEGA® Attack
-# license is GPLv3, see https://www.gnu.org/licenses/gpl-3.0.en.html
+# This file is a Jlsca implementation of Dr. Ilya Kizhvatov's XMEGA® Attack
+# It was inspired by the ChipWhisperer Tutorial A6
+# https://wiki.newae.com/Tutorial_A6_Replication_of_Ilya_Kizhvatov%27s_XMEGA%C2%AE_Attack
+# As stated there, you will need a copy of the paper entitled "Side Channel
+# Analysis of AVR XMEGA Crypto Engine"
+
+# The `xor` parameter controls whether we xor with the previousKeyByte, or not.
+# If we do, then (assuming the first keybyte is correct) we will get the whole
+# correct key. If we do not set it, the output will be the xors of keybyte
+# pairs.
 #
+# The `rhme3` parameter controls which pair of keybytes are xored together. In
+# Ilya's paper, the keybytes are processed in linear order, but in the XMEGA
+# A3U and A4U processors, they are processed in ShiftRows order.
+
+# license is GPLv3, see https://www.gnu.org/licenses/gpl-3.0.en.html
 # Author: Jonathan Beverley
 
 using ..Aes
 using ..Trs
 
-export AesIlyaAttack, ilyaRankCallBack, flipHW
+export AesTwoRoundAttack, twoRoundRankCallBack, flipHW
 
-type AesIlyaAttack <: AesAttack
+type AesTwoRoundAttack <: AesAttack
     mode::AesMode
     keyLength::AesKeyLength
     direction::Direction
@@ -17,12 +30,12 @@ type AesIlyaAttack <: AesAttack
     knownKey::Array{UInt8,1}
     previousKeyByte # untyped to allow use of `nothing`
     rhme3::Bool
-    function AesIlyaAttack()
+    function AesTwoRoundAttack()
         return new(CIPHER, KL128, FORWARD, false, Aes.sbox, [], 0, false)
     end
 end
 
-function ilyaRankCallBack(params::AesIlyaAttack, rankData::RankData, keyOffsets::Vector{Int64})
+function twoRoundRankCallBack(params::AesTwoRoundAttack, rankData::RankData, keyOffsets::Vector{Int64})
     phase = length(rankData.combinedScores)
     target = length(rankData.combinedScores[phase])
     orderedArrayOfFloats = rankData.combinedScores[phase][target]
@@ -42,7 +55,7 @@ end
 
 # Need a new target
 type TwoRoundTarget <: Target{UInt16,UInt8,UInt8}
-    params::AesIlyaAttack
+    params::AesTwoRoundAttack
 end
 function target(a::TwoRoundTarget, data::UInt16, guess::UInt8)
     prevData = UInt8(data>>8)
@@ -57,12 +70,8 @@ function target(a::TwoRoundTarget, data::UInt16, guess::UInt8)
 end
 show(io::IO, a::TwoRoundTarget) = print(io, "Two-round target: (Pᵢ₋₁ ⊻ Kᵢ₋₁) ⊻ (Pᵢ ⊻ Kᵢ)")
 
-function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
-    if params.rhme3
-        params.previousKeyByte = nothing
-    else
-        params.previousKeyByte = UInt8(phase-1)
-    end
+function getTargets(params::AesTwoRoundAttack, phase::Int64, phaseInput::Array{UInt8,1}) 
+    params.previousKeyByte = UInt8(phase-1)
 
     # We can't set previous key-bytes yet, because we don't know them, do that later
     targetfn = TwoRoundTarget(params)
@@ -70,19 +79,19 @@ function getTargets(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8}
 end
 
 # Optional, ideally it should just work, but we might have to brute-force the first byte
-numberOfPhases(params::AesIlyaAttack) = 1
-numberOfTargets(params::AesIlyaAttack, phase::Int) = 16
+numberOfPhases(params::AesTwoRoundAttack) = 1
+numberOfTargets(params::AesTwoRoundAttack, phase::Int) = 16
 
-show(io::IO, a::AesIlyaAttack) = print(io, "AES two-byte Ilya attack")
+show(io::IO, a::AesTwoRoundAttack) = print(io, "AES two-round attack")
 
-function printParameters(params::AesIlyaAttack)
+function printParameters(params::AesTwoRoundAttack)
     @printf("mode:       %s\n", string(params.mode))
     @printf("key length: %s\n", string(params.keyLength))
     @printf("direction:  %s\n", string(params.direction))
     @printf("known key:  %s\n", string(params.knownKey))
 end
 
-function recoverKey(params::AesIlyaAttack, phaseInputOrig::Vector{UInt8})
+function recoverKey(params::AesTwoRoundAttack, phaseInputOrig::Vector{UInt8})
     if params.rhme3
         result = reshape(Aes.InvShiftRows(reshape(phaseInputOrig[1:16],(4,4))), 16)
     else
@@ -94,10 +103,10 @@ function recoverKey(params::AesIlyaAttack, phaseInputOrig::Vector{UInt8})
     return result
 end
 
-function twoRoundFilter(params::AesIlyaAttack, data::Vector{UInt8})
-    # called once per row, to return the "data" we'll use...
-    # we return pairs of xor'd bytes to prep for above...
-    # For the first round, use 0x0 as the previous datum
+function twoRoundFilter(params::AesTwoRoundAttack, data::Vector{UInt8})
+    # Called once per row, to return the "data" we'll use...
+    # We return pairs of xored bytes to prep for above...
+    # For the first round, assume 0x0 as the previous datum
     if params.rhme3
         data = reshape(Aes.ShiftRows(reshape(data[1:16],(4,4))), 16)
     end
@@ -107,7 +116,7 @@ function twoRoundFilter(params::AesIlyaAttack, data::Vector{UInt8})
                )
 end
 
-function getDataPass(params::AesIlyaAttack, phase::Int, phaseInput::Vector{UInt8})
+function getDataPass(params::AesTwoRoundAttack, phase::Int, phaseInput::Vector{UInt8})
     return Nullable(x -> twoRoundFilter(params, x))
 end
 
